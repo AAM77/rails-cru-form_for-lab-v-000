9b44e35ed15bd77d23cb67c203dd9cc133f3042c
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-08-05 17:20:47 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -19,7 +19,7 @@ class ArtistsController < ApplicationController
   
   def update
     @artist = Artist.find_by_id(params[:id])
-    @artist.update(artist_params(:bio))
+    @artist.update(artist_params(:name, :bio))
     redirect_to artist_path(@artist)
   end
   
